d87329a06c6f3dc28aec94b92568cf7ce2865e61
Alexander Ageichenko <wackalooon@gmail.com>
2015-10-19 23:00:23 +0300

    test relise v2

--- GraphDesigner/GraphDesigner/Properties/Settings.Designer.cs
+++ GraphDesigner/GraphDesigner/Properties/Settings.Designer.cs
@@ -70,5 +70,27 @@ namespace GraphDesigner.Properties {
                 this["ShortPathNodeColor"] = value;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("\"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|GraphDB.mdf;I" +
+            "ntegrated Security=True\"")]
+        public string ConString {
+            get {
+                return ((string)(this["ConString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\GraphDB.mdf;I" +
+            "ntegrated Security=True")]
+        public string GraphDBConnectionString {
+            get {
+                return ((string)(this["GraphDBConnectionString"]));
+            }
+        }
     }
 }
