ed2f489c9e710e946176a7aca4667ef99d31c5c9
Alexander Ageichenko <wackalooon@gmail.com>
2015-10-19 21:28:38 +0300

    test version

--- GraphDesigner/GraphDesigner/AgeichenkoTestTask.Designer.cs
+++ GraphDesigner/GraphDesigner/AgeichenkoTestTask.Designer.cs
@@ -326,6 +326,7 @@
             this.Controls.Add(this.buttonShortWay);
             this.Controls.Add(this.pictureBoxGraph);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "AgeichenkoTestTask";
             this.Text = "Graph Designer";
